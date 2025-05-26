bdbb28fe3db3309f054343ad601a4491154596eb
Matthias Richter <matthias.richter@inovex.de>
2025-05-26 10:08:42 +0200

    Add config: sqlite, git aliases

--- hosts/siona/home/matthias.nix
+++ hosts/siona/home/matthias.nix
@@ -25,6 +25,11 @@ in {
     just
   ];
 
+  home.file.".sqliterc".text = ''
+    .headers ON
+    .mode box
+  '';
+
   programs.home-manager.enable = true;
 
   programs.git = {
@@ -41,10 +46,17 @@ in {
     aliases = {
       diffc = "diff --cached";
       co = "checkout";
+      sw = "switch";
+      swi = /*bash*/"!git switch $(git branch -a | sed 's:remotes/origin/::g' | awk '!s[$0]{print}{s[$0]++}' | fzf)";
       ci = "commit";
       st = "status -sb";
       br = "branch -a";
       hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+      adog = "log --all --decorate -tonline --graph";
+      rbi = "rebase -i";
+      unstage = "reset HEAD --";
+      sl = "stash list --pretty=format:\"%C(red)%h%C(reset) - %C(bold magenta)(%gd)%C(reset) %<(70,trunc)%s %C(green)(%cr) (%C(bold blue)<%an>%C(reset)\"";
+      theirs = "checkout --theirs";
     };
     ignores = [ "*.swo" "*.swp" ];
   };
