b3500fbb6008fb22b96c9e832a112146ac8ad398
Matthias Richter <vrld@vrld.org>
2025-09-09 19:49:52 +0200

    flake update

--- components/home/ssh.nix
+++ components/home/ssh.nix
@@ -2,8 +2,19 @@
 
   programs.ssh = {
     enable = true;
-    controlMaster = "auto";
-    addKeysToAgent = "ask";
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "ask";
+      controlMaster = "auto"; # opportunistic multiplexing: try to use existing connection, fall back to creating a new one
+      controlPath = "~/.ssh/%C.control";
+      controlPersist = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
   };
 
 }
